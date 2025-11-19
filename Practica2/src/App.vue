34b014308b4dfb900b8419c40f92ee1afe747e08
imanol.manero <imanol.manero.51413@ikasle.egibide.org>
2025-11-19 10:04:53 +0100

    cosas

---
+++ Practica2/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import nombreCompleto from './components/Ej1.vue'
+  import vbindPractica from './components/Ej2.vue'
+</script>
+
+<template>
+  <h1>Ejercicio 1</h1>
+  <nombreCompleto></nombreCompleto>
+  <br/>
+  <br/>
+  <h1>Ejercicio 2</h1>
+  <vbindPractica></vbindPractica>
+  <br/>
+  <br/>
+  
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Practica2/src/components/Ej1.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import {ref,computed} from 'vue'
+
+    const nombre = ref("")
+    const apellido = ref("")
+
+    const nombreCompleto = computed(() => {
+        return nombre.value + " " + apellido.value
+    }
+
+    )
+</script>
+
+<template>
+    <h2>Tu nombre completo es: {{ nombreCompleto }}</h2>
+    Nombre: <input type="text" v-model="nombre" placeholder="nombre">
+    Apellido:<input type="text" v-model="apellido" placeholder="apellido">
+</template>
